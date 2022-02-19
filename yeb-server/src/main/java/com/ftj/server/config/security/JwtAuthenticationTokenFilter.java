cad79539c55e1258ceeb4f780ba3b1c5198fba94
21seu <1693376841@qq.com>
2022-02-19 23:15:51 +0800

    配置Security登录授权过滤器

---
+++ yeb-server/src/main/java/com/ftj/server/config/security/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,56 @@
+package com.ftj.server.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by fengtj on 2022/2/19 14:33
+ */
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Value("${jwt.tokenHeader}")
+    private String tokenHeader;
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String header = httpServletRequest.getHeader(tokenHeader);
+        //存在token
+        if (null != header && header.startsWith(tokenHead)) {
+            String authToken = header.substring(tokenHead.length());
+            String username = jwtTokenUtil.getUserNameFromToken(authToken);
+            //token存在用户名但是未登录
+            if (null != username && null == SecurityContextHolder.getContext().getAuthentication()) {
+                //登录
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                //验证token是否有效果，重新设置用户对象
+                if (jwtTokenUtil.validateToken(tokenHead, userDetails)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
---
+++ yeb-server/src/main/java/com/ftj/server/config/security/SecurityConfig.java
@@ -0,0 +1,81 @@
+package com.ftj.server.config.security;
+
+import com.ftj.server.pojo.Admin;
+import com.ftj.server.service.IAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Security 配置类
+ * Created by fengtj on 2022/2/19 14:12
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private IAdminService adminService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //使用jwt不需要csrf
+        http.csrf()
+                .disable()
+                //基于token不需要session
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                //允许登录访问
+                .antMatchers("/login", "/logout")
+                .permitAll()
+                //除了上面，所有请求都要认证
+                .anyRequest()
+                .authenticated()
+                .and()
+                .headers()
+                .cacheControl();
+        //添加jwt，登录授权过滤器
+        http.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        //添加自动逸未授权和未登录的结果返回
+        http.exceptionHandling()
+                .accessDeniedHandler()
+                .authenticationEntryPoint();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            Admin admin = adminService.getAdminByUserName(username);
+            if (null != admin) {
+                return admin;
+            }
+            return null;
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() {
+        return new JwtAuthenticationTokenFilter();
+    }
+
+}
