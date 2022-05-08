52f9bd250601e90dfaf0e14337e346158f0c8a6b
21seu <1693376841@qq.com>
2022-05-08 14:28:34 +0800

    职位管理功能实现

--- yeb-server/src/main/java/com/ftj/server/pojo/Position.java
+++ yeb-server/src/main/java/com/ftj/server/pojo/Position.java
@@ -3,6 +3,7 @@ package com.ftj.server.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author fengtj
@@ -24,7 +25,7 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("t_position")
-@ApiModel(value="Position对象", description="")
+@ApiModel(value = "Position对象", description = "")
 public class Position implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -37,6 +38,7 @@ public class Position implements Serializable {
     private String name;
 
     @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Shanghai")
     private LocalDateTime createDate;
 
     @ApiModelProperty(value = "是否启用")
