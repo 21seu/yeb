e39fae847f91ee61e9ef7225a1b1260892986776
21seu <1693376841@qq.com>
2022-05-10 00:08:52 +0800

    创建邮件发送服务

---
+++ yeb-mail/src/main/java/com/ftj/MailApplication.java
@@ -0,0 +1,15 @@
+package com.ftj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by fengtj on 2021/8/27 1:21
+ */
+@SpringBootApplication
+public class MailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailApplication.class, args);
+    }
+}
